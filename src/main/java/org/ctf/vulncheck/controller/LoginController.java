0eec3b4977aa4cd0830db567ca25241c011a399a
shukuang <aashukuang@gmail.com>
2025-04-16 19:17:57 +0800

    chage spel

--- src/main/java/org/ctf/vulncheck/controller/LoginController.java
+++ src/main/java/org/ctf/vulncheck/controller/LoginController.java
@@ -2,10 +2,13 @@ package org.ctf.vulncheck.controller;
 
 import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.expression.ExpressionParser;
+import org.springframework.expression.spel.standard.SpelExpressionParser;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.io.ByteArrayInputStream;
 import java.io.IOException;
@@ -18,30 +21,9 @@ import static org.springframework.web.util.WebUtils.getCookie;
 @Controller
 public class LoginController {
 
-    @GetMapping({"/", "/login"})
-    public String loginPage(HttpServletRequest request) {
-        Cookie cookie = getCookie(request, "rememberMe");
-
-        if (null == cookie) {
-            return "No rememberMe cookie. Right?";
-        }
-        String rememberMe = cookie.getValue();
-        byte[] decoded = Base64.getDecoder().decode(rememberMe);
-
-        ByteArrayInputStream bytes = new ByteArrayInputStream(decoded);
-
-        try {
-            ObjectInputStream in = new ObjectInputStream(bytes);  // throw InvalidClassException
-            in.readObject();
-            in.close();
-        } catch (InvalidClassException e) {
-            return e.toString();
-        } catch (ClassNotFoundException e) {
-            throw new RuntimeException(e);
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
-
-        return "I'm very OK.";
+    @RequestMapping("/spel/vuln1")
+    public String spel_vuln1(String value) {
+        ExpressionParser parser = new SpelExpressionParser();
+        return parser.parseExpression(value).getValue().toString();
     }
 }
